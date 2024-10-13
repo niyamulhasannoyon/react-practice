a6af466e3ed3b129d3c04e956566bb5c5b9ce5ce
MD. Niyamul Hasan <niyamulhasan1089@gmail.com>
2024-10-13 22:32:33 +0600

    react-practice eith rabbil

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>learn in React</h1>
+        <input type="text" name="name" id="name" placeholder="Enter Your Name" />
+        <button type="button">Submit</button>
+      </div>
+    </>
+  )
+}
+
+export default App
